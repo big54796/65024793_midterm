69769b763529e248be2a719f585e215a441cc24f
big54796 <121143207+big54796@users.noreply.github.com>
2023-08-17 11:20:54 +0700

    Add files via upload

---
+++ 65024793.java
@@ -0,0 +1,90 @@
+public class 65024793 {
+    public static void main(String[] args) {
+        Product chair = new Product("chair", 500.0);
+        chair.displayInfo();
+
+        System.out.println();
+
+        Electronics electronics = new Electronics("Electronics", 20000.0, "Samsung", "Neo QLED");
+        electronics.displayInfo();
+
+        System.out.println();
+
+        Phone iphone = new Phone("Iphone", 40000.0, "Apple", "Iphone 99", "ios");
+        iphone.displayInfo();
+
+        System.out.println();
+
+        Book book = new Book("OOP Programming", 250.0, "John Doe", 300);
+        book.displayInfo();
+    }
+}
+class Product {
+    String name;
+    double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public void displayInfo() {
+        System.out.println("Product Name : " + name);
+        System.out.println("Product Price : " + price);
+    }
+}
+
+class Electronics extends Product {
+    String brand;
+    String model;
+
+    public Electronics(String name, double price, String brand, String model) {
+        super(name, price);
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Brand : " + brand);
+        System.out.println("Model : " + model);
+    }
+}
+
+class Phone extends Product {
+    String brand;
+    String model;
+    String os;
+
+    public Phone(String name, double price, String brand, String model, String os) {
+        super(name, price);
+        this.brand = brand;
+        this.model = model;
+        this.os = os;
+    }
+
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Brand : " + brand);
+        System.out.println("Model : " + model);
+        System.out.println("Operating System : " + os);
+    }
+}
+
+class Book extends Product {
+    String author;
+    int numOfPages;
+
+    public Book(String name, double price, String author, int numOfPages) {
+        super(name, price);
+        this.author = author;
+        this.numOfPages = numOfPages;
+    }
+
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Author : " + author);
+        System.out.println("Number of Pages : " + numOfPages);
+    }
+}
+
